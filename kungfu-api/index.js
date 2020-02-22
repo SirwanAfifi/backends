ef308eb922737374bfde8233f2be6fc52ab06b83
sirwan <sir1afifi@gmail.com>
2020-02-23 01:58:41 +0330

    Add Simple GraphQL server with Apollo Server

---
+++ kungfu-api/index.js
@@ -0,0 +1,44 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Movie {
+    title: String
+    releaseDate: String
+    rating: Int
+  }
+
+  type Query {
+    movies: [Movie]
+  }
+`;
+
+// Fake data
+const movies = [
+  {
+    title: "5 Deadly Venoms",
+    releaseDate: "10-10-1983",
+    rating: 5
+  },
+  {
+    title: "36th Chamber",
+    releaseDate: "10-10-1993",
+    rating: 5
+  }
+];
+
+const resolvers = {
+  Query: {
+    movies: () => {
+      return movies;
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server started at ${url}`);
+});
