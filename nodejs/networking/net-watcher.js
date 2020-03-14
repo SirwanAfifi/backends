5bbdf9cdeac487b384b271bc862de7814f19bcdb
sirwan <sir1afifi@gmail.com>
2020-03-14 18:19:07 +0330

    Add socket

---
+++ nodejs/networking/net-watcher.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const net = require("net");
+const filename = process.argv[2];
+
+if (!filename) {
+  throw Error("Error: No file specified.");
+}
+
+net
+  .createServer(connection => {
+    // Reporting
+    console.log("Subscriber connected.");
+    connection.write(`Now watching ${filename} for changes...\n`);
+
+    // Watcher setup.
+    const watcher = fs.watch(filename, () =>
+      connection.write(`File changed: ${new Date()}\n`)
+    );
+
+    // Cleanup.
+    connection.on("close", () => {
+      console.log("Subscriber disconnected.");
+      watcher.close();
+    });
+  })
+  .listen(60300, () => console.log("Listening for subscribers..."));
