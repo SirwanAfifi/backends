e58d7c74649a89ec20209015bec5ef2c2d6df777
sirwan <sir1afifi@gmail.com>
2020-03-15 19:52:55 +0330

    Add Tray app

--- desktop/tasky/public/electron.js
+++ desktop/tasky/public/electron.js
@@ -1,17 +1,46 @@
+const path = require("path");
 const electron = require("electron");
 
-const { app, BrowserWindow, ipcMain } = electron;
+const { app, BrowserWindow, ipcMain, Tray } = electron;
 
 const isDev = require("electron-is-dev");
 
 let mainWindow;
+let tray;
 
 app.on("ready", () => {
-  mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({
+    height: 500,
+    width: 300,
+    frame: false,
+    resizable: false,
+    show: false
+  });
 
   mainWindow.loadURL(
     isDev
       ? "http://localhost:3000"
       : `file://${path.join(__dirname, "../build/index.html")}`
   );
+
+  const iconName = process.platform === "win32" ? "icon.png" : "icon.png";
+  const iconPath = path.join(__dirname, `./${iconName}`);
+  tray = new Tray(iconPath);
+  tray.on("click", (event, bounds) => {
+    const { x, y } = bounds;
+    const { height, width } = mainWindow.getBounds();
+
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      const yPosition = process.platform === "darwin" ? y : y - height;
+      mainWindow.setBounds({
+        x: x - width / 2,
+        y: yPosition,
+        height,
+        width
+      });
+      mainWindow.show();
+    }
+  });
 });
