0183e0f82a6d137d2c52c8e12e0d6b8ef8788a39
sirwan <sir1afifi@gmail.com>
2020-03-20 21:40:23 +0330

    Add hubs

--- dotnet/signalr/Startup.cs
+++ dotnet/signalr/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using signalr.hubs;
 
 namespace signalr
 {
@@ -16,7 +17,7 @@ namespace signalr
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,6 +32,7 @@ namespace signalr
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapHub<ChatHub>("/chatHub");
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
