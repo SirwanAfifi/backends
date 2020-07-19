edf0c8c75688510790a2f647216177afeebdd991
Sirwan <sir1afifi@gmail.com>
2020-07-19 14:00:00 +0100

    Add Closure

--- dotnet/fp/Program.cs
+++ dotnet/fp/Program.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace fp
 {
     class Program
     {
+        private static IList<int> numbers = Enumerable.Range(1, 20).ToList();
+        private static int value = 5;
+        private static Func<int, int> Adder = x => x += value;
         static void Main(string[] args)
         {
+            var result = numbers.Sum(Adder);
+            Console.WriteLine(result);
+
             var counter = CreateCounter();
             var num1 = counter();
             var num2 = counter();
