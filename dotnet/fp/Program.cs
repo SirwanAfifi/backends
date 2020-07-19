3a7268ad5b960761ddc36022d0839604a9d55041
Sirwan <sir1afifi@gmail.com>
2020-07-19 14:00:00 +0100

    Add fp project

---
+++ dotnet/fp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace fp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = CreateCounter();
+            var num1 = counter();
+            var num2 = counter();
+            var num3 = counter();
+            System.Console.WriteLine(num3);
+        }
+
+        static Func<int> CreateCounter()
+        {
+            var num = 0;
+            return () => num++;
+        }
+    }
+}
